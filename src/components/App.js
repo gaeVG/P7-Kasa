bff22f5ee83b4619178e28b7a58d80cd88f14a67
Thiebaultnicolas <nicolasthiebault80870@yahoo.com>
2022-10-20 16:10:57 +0200

    2 premier composants

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import logo from '../logo.svg';
+import '../App.css';
+import {Header} from './Header.js';
+import {Banner} from './Banner.js';
+
+function App() {
+  return (
+    <><Header /><Banner /></>
+  );
+  
+}
+
+
+export default App;
+
---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import '../styles/Banner.css'
+
+export function Banner () {
+    const Tittle = 'Chez vous, partout et ailleurs'
+    return (
+             <div className="backgroundimage">
+                   <h1 className='lmj-title-banner-montain'>{Tittle}</h1>
+            </div>  
+        
+    )
+}
