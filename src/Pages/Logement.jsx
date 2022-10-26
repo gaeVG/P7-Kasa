6ffe2fd87bb36e222e9bf127f6e2055c488d3ecb
Thiebaultnicolas <nicolasthiebault80870@yahoo.com>
2022-10-26 19:37:25 +0200

    .filter logement

--- src/Pages/Logement.jsx
+++ src/Pages/Logement.jsx
@@ -1,18 +1,29 @@
+import { useParams } from 'react-router';
 import api from '../API/data.json';
 import {Caroussel} from '../components/Caroussel.jsx';
 import {Faq} from '../components/Faq.jsx';
 
 
 
-function Logement() {
-    return (
-      <>
-       <div className='carousel'>
-      {api.map((slider) => <Caroussel key={slider.id} pictures={slider.pictures} id={slider.id} />)}
-      </div>
-      </>
-    )
-    
-  }
+
+function Logement(){
+  let {id} = useParams()
+
+  let test = api.filter( logement => logement.id == id )
+
+  let logement = test[0]
+
+  return (
+    <>
+      <h1>{logement.title}</h1>
+      {
+        logement.pictures.map((picture) =>{
+          return <img src={picture} />
+        })
+      }
+    </>
+  )
+};
   
-  export default Logement;
+  
+export default Logement;
