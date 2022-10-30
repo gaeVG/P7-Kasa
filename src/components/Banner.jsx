33c1904ce85f5cf5dcfc5b5c46761828181af26e
Thiebaultnicolas <nicolasthiebault80870@yahoo.com>
2022-10-30 17:48:14 +0100

    banner responsive en cour

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,10 +3,14 @@ import '../styles/Banner.css'
 
 export function Banner ({image, titre}) {
     return (
+       
             <div className="backgroundimage">
                 <img src={image} alt='image banniere accueil' className='imageBanniereAccueil' />    
-                <h1 className='title-banner-montain'>{titre}</h1>   
+                <h1 className='title-banner-montain'>{titre}</h1>
             </div>  
+        
         
     )
 }
+
+
