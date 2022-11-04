bee35af35407022bacc04047d0d30be49982b821
Thiebaultnicolas <nicolasthiebault80870@yahoo.com>
2022-11-04 17:43:14 +0100

    c en C

--- src/components/carts.jsx
+++ src/components/carts.jsx
@@ -3,7 +3,6 @@ import '../styles/Carts.css'
 
 export function Carts({id, cover, title}) {
     return (
-
         <div className='logements' id={id} >
             <img src={cover} alt ="image carte accueil" className='imageCarts'></img>
             <p className='tittleCarts'> {title} </p> 
